cc4f2021d7d4359c777ac1368ecf598400f327ad
Ayrton Mugnaini <yrto@outlook.com>
2021-06-18 02:19:43 -0300

    Fetch products from Magalu with selenium

--- selenium.js
+++ selenium.js
@@ -1,17 +1,55 @@
-import { Builder, By, Key, until } from "selenium-webdriver";
+import { Builder, By, Key } from "selenium-webdriver";
+
+const fetchProductLinks = async (driver, searchText, arrayLength) => {
+  let linkList = [];
+  try {
+    await driver.get("https://www.magazineluiza.com.br/");
+    const searchBar = await driver.findElement(By.id("inpHeaderSearch"));
+    await searchBar.sendKeys(searchText, Key.RETURN);
+    await driver.sleep(4000);
+    const webElementArray = await driver.findElements({
+      className: "product-li",
+    });
+    webElementArray.slice(0, arrayLength).map(async (element) => {
+      const link = await element.getAttribute("href");
+      linkList.push(link);
+    });
+    await driver.sleep(2000);
+    console.log(linkList);
+    return linkList;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const fetchProductInfo = async (driver, url) => {
+  await driver.get(url);
+  let productTitle = await driver.findElement(By.css("h1")).getText();
+  let productPrice = await driver
+    .findElement(By.className("price-template__text"))
+    .getText();
+  let productInfo = {
+    name: productTitle,
+    price: productPrice,
+  };
+  return productInfo;
+};
+
+const nonConcurrentMap = async (driver, arr, fn) => {
+  const data = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    const current = arr[i];
+    data.push(await fn(driver, current));
+  }
+  console.log(data);
+  return data;
+};
 
 const main = async () => {
   const driver = new Builder().forBrowser("firefox").build();
-  try {
-    await driver.get("https://google.com");
-    const myElement = await driver.findElement(By.name("q"));
-    await myElement.sendKeys("webdriver", Key.RETURN);
-    await driver.wait(until.titleIs("webdriver - Google Search"), 5000);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    await driver.quit();
-  }
+  const links = await fetchProductLinks(driver, "colher", 3);
+  await nonConcurrentMap(driver, links, fetchProductInfo);
+  await driver.quit();
 };
 
 main();
